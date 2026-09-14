f8c6111a78a301fc34ed3f63b8b02d0154b3d907
Risktide2 <144982113+Risktide2@users.noreply.github.com>
2026-09-14 15:02:41 +1200

    tutorial complete, terrain 3d addon added

--- green-valley-reboot/scripts/player.gd
+++ green-valley-reboot/scripts/player.gd
@@ -1,14 +1,37 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var truespeed = 5.0
 const JUMP_VELOCITY = 4.5
 const SPRINT_VELOCITY = 2
 
+var walkspeed = 5
+var crawlspeed = 2.5
+
+var crouch_height = 0.5
+var stand_height = 2.0
+var crouching = false
+
 @onready var pivot: Node3D = $CameraOrigin
 @export var sens = 0.5
+@onready var footstepaudio_3d: AudioStreamPlayer3D = %Footstepaudio3d
+
+var foot_step_play := true
+var footstep_landed
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func crouch():
+	if Input.is_action_just_pressed("crouch"):
+		crouching = !crouching
+		if crouching:
+			$AnimationPlayer.play("StandingToCrouch")
+			truespeed = crawlspeed
+		else:
+			$AnimationPlayer.play_backwards("StandingToCrouch")
+			truespeed = walkspeed
+	
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -17,6 +40,9 @@ func _input(event):
 		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
 
 func _physics_process(delta: float) -> void:
+	
+	crouch()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -30,17 +56,24 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * truespeed
+		velocity.z = direction.z * truespeed
 		#check if sprinting
 		if Input.is_action_pressed("sprint"):
 			#we are sprinting
 			velocity.z *= SPRINT_VELOCITY
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, truespeed)
+		velocity.z = move_toward(velocity.z, 0, truespeed)
 
 	move_and_slide()
+	
+	if not footstep_landed and is_on_floor(): #landed
+		%Footstepaudio3d.play
+	elif footstep_landed and not is_on_floor(): #jumped
+		%Footstepaudio3d.play
+	footstep_landed = is_on_floor()
+		
 
 
 func toggle(body: Variant) -> void:
